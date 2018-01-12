46598b4d2aea7b605dc921990ff90f7e906109bd
Ferenc Gutierrez <ferenc.g87@gmail.com>
2018-01-12 11:39:58 -0600

    Added additional controllers and models

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
+var db = require("./models");
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -18,8 +20,10 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgersController.js");
 
-app.use("/", routes);
+require("./controllers/burgersController.js")(app);
 
-app.listen(port);
+db.sync({ force: true}).then(function(){
+    app.listen(port);
+})
+
